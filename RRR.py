f506977b75db0e7f82f98dca0859f510a6e828b3
Abraham Nassiff <anassiff@gmail.com>
2016-02-14 13:40:06 -0430

    Fixed problem with long definitions from /ud

--- RRR.py
+++ RRR.py
@@ -1027,26 +1027,31 @@ def ud(message):
 	if intime(message):
 		cid = getCID(message)
 		isSearch = False
-		search = getContent(message)
-		if search != "-?" and search:
-			url = "http://api.urbandictionary.com/v0/define?term=" + search
-			request = requests.get(url)
-			if request.status_code == 200:
-				data = request.json()
-				if data['result_type'] == 'exact':
-					definition = data['list'][0]['definition']
-					permalink = data['list'][0]['permalink']
-					example = data['list'][0]['example']
-					word = data['list'][0]['word']
-					definition = definition.rstrip('].').lstrip('[')
-					message_ub = u"\t`Urban Dictionary Definition`\n\n"u" \t\t`\t\t> {word}:`\n\n"u"`{definition}`\n\n\n"u"`Example:\n{example}`\n\n"u"`If you want more info on this, feel free to visit this link:` {permalink}".format(word=word, definition=definition, example=example, permalink=permalink)
-					bot.send_message(cid, message_ub, parse_mode="Markdown", disable_web_page_preview=True)
+		if not cid == degeneratesgroup:
+			search = getContent(message)
+			if search != "-?" and search:
+				url = "http://api.urbandictionary.com/v0/define?term=" + search
+				request = requests.get(url)
+				if request.status_code == 200:
+					data = request.json()
+					if data['result_type'] == 'exact':
+						if len(data['list'][0]['definition']) < 1000:
+							definition = data['list'][0]['definition'] + "\n\n\n"
+							example = "Example:\n" + data['list'][0]['example'] + "\n\n"
+						else:
+							definition = "Some idiot thought it was a nice idea to write a wall of text as definition, please refer to the link below.\n"
+							example = ""
+						permalink = data['list'][0]['permalink']
+						word = data['list'][0]['word']
+						definition = definition.rstrip('].').lstrip('[')
+						message_ub = u"\t`Urban Dictionary Definition`\n\n"u" \t\t`\t\t> {word}:`\n\n"u"`{definition}`"u"`{example}`"u"[If you want more info on this, feel free to visit this link]({permalink})".format(word=word, definition=definition, example=example, permalink=permalink)
+						bot.send_message(cid, message_ub, parse_mode="Markdown", disable_web_page_preview=True)
+					else:
+						bot.send_message(cid, u"`No results found for {search}.`".format(search=search), parse_mode="Markdown")
 				else:
-					bot.send_message(cid, u"`No results found for {search}.`".format(search=search), parse_mode="Markdown")
+					bot.send_message(cid, '`{error}`'.format(error=data.raise_for_status()), parse_mode="Markdown")
 			else:
-				bot.send_message(cid, '`{error}`'.format(error=data.raise_for_status()), parse_mode="Markdown")
-		else:
-			bot.send_message(cid, "`Follow this command with your search and I will show you the definition of it.`", parse_mode="Markdown")
+				bot.send_message(cid, "`Follow this command with your search and I will show you the definition of it.`", parse_mode="Markdown")
 
 # Reddit
 
