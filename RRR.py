33993d26fad4ce17bab26406019785ff2cbd0656
Abraham Nassiff <anassiff@gmail.com>
2016-02-13 21:56:36 -0430

    Switched dice roll to correct order

--- RRR.py
+++ RRR.py
@@ -766,29 +766,29 @@ def dice_roll(message):
 		faces = getContent(message)
 		cid = getCID(message)
 		if faces != "-?" or not faces:
-			if faces == "4" or faces == "4d":
+			if faces == "4" or faces == "d4":
 				roll = random.randrange(1, 5, 1)
 				bot.send_message(cid, "`" + str(roll) + "`", parse_mode="Markdown")
-			elif faces == "6" or faces == "6d" or not faces:
+			elif faces == "6" or faces == "d6" or not faces:
 				roll = random.randrange(1, 7, 1)
 				bot.send_message(cid, "`" + str(roll) + "`", parse_mode="Markdown")
-			elif faces == "8" or faces == "8d":
+			elif faces == "8" or faces == "d8":
 				roll = random.randrange(1, 9, 1)
 				bot.send_message(cid, "`" + str(roll) + "`", parse_mode="Markdown")
-			elif faces == "10" or faces == "10d":
+			elif faces == "10" or faces == "d10":
 				roll = random.randrange(1, 11, 1)
 				bot.send_message(cid, "`" + str(roll) + "`", parse_mode="Markdown")
-			elif faces == "12" or faces == "12d":
+			elif faces == "12" or faces == "d12":
 				roll = random.randrange(1, 13, 1)
 				bot.send_message(cid, "`" + str(roll) + "`", parse_mode="Markdown")
-			elif faces == "20" or faces == "20d":
+			elif faces == "20" or faces == "d20":
 				roll = random.randrange(1, 21, 1)
 				bot.send_message(cid, "`" + str(roll) + "`", parse_mode="Markdown")
 			else:
 				roll = random.randrange(1, 7, 1)
 				bot.send_message(cid, "`No valid dice detected, rolling a 6d:\n\n    " + str(roll) + "\n\n/diceroll -?`", parse_mode="Markdown")
 		else:
-			bot.reply_to(message, "`Valid dices:\n4d\n6d\n10d\n12d\n20d\n\n  /diceroll 20d`", parse_mode="Markdown")
+			bot.reply_to(message, "`Valid dices:\nd4\nd6\nd10\nd12\nd20\n\n  /diceroll d20`", parse_mode="Markdown")
 
 # Roll
 
