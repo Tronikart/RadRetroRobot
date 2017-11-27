26f2e4ac563c2988bb120ef82daa14e6b6927cc0
Abraham Nassiff <anassiff@gmail.com>
2017-11-27 00:32:49 -0400

    Gfycat now sent with captions

--- plugins/autogfycat.py
+++ plugins/autogfycat.py
@@ -5,7 +5,8 @@ print ('loading ' + __name__)
 def action(bot, update):
 	cid = getCID(update)
 	try:
-		bot.send_document(cid, getGfy(update.message.text))
+		url, title = getGfy(update.message.text)
+		bot.send_document(cid, url, caption=title)
 	except:
 		pass
 
