c309d7e04696ea41afa5925cd2dd9b0f435901e0
Abraham Nassiff <anassiff@gmail.com>
2017-11-27 00:23:17 -0400

    captions taken from

--- plugins/autoImgur.py
+++ plugins/autoImgur.py
@@ -3,12 +3,16 @@ from utils import *
 print ('loading ' + __name__)
 
 def getImgur(url):
-	url = re.findall(r'(http[s]?://i.imgur.com/.+.gif[v]?)', url)[0].replace('gifv', 'mp4').replace('gif', 'mp4')
-	return url
+	url = re.findall(r'(http[s]?://i.imgur.com/.+.gif[v]?)', url)[0]
+	request = requests.get(url)
+	title = BeautifulSoup(request.text, 'html.parser').title.text
+	url = url.replace('gifv', 'mp4').replace('gif', 'mp4')
+	return url, title
 
 def action(bot, update):
 	cid = getCID(update)
-	bot.send_document(cid, getImgur(update.message.text))
+	url, title = getImgur(update.message.text)
+	bot.send_document(cid, url, caption=title)
 
 info = {	'triggers' 	:	r'.*http[s]?://i.imgur.com/.+.gif[v]?.*',
 			'active'	: 	True,
