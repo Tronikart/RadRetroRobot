3f721f1b054de2ed47a41fb10a0f931fca116c25
Abraham Nassiff <anassiff@gmail.com>
2017-11-28 00:43:13 -0400

    autoReddit improvement handling link posts

--- plugins/autoImgur.py
+++ plugins/autoImgur.py
@@ -2,14 +2,6 @@ from utils import *
 
 print ('loading ' + __name__)
 
-def getImgur(url):
-	url = re.findall(r'(http[s]?://i.imgur.com/.+.gif[v]?)', url)[0]
-	url = url if url[-1] == 'v' else url + "v"
-	request = requests.get(url)
-	title = BeautifulSoup(request.text, 'html.parser').title.text
-	url = url.replace('gifv', 'mp4').replace('gif', 'mp4')
-	return url, title
-
 def action(bot, update):
 	cid = getCID(update)
 	url, title = getImgur(update.message.text)
@@ -18,3 +10,5 @@ def action(bot, update):
 info = {	'triggers' 	:	r'.*http[s]?://i.imgur.com/.+.gif[v]?.*',
 			'active'	: 	True,
 			'admin'		: 	False}
+
+			
--- plugins/autoreddit.py
+++ plugins/autoreddit.py
@@ -42,17 +42,36 @@ def action(bot, update):
 								self_post_long = "`by /u/"+poster+"`\n\n`"+"The text from this post excedes my text message limit\n\n"+"<"+isNsfw+">" +" | "+str(up)+" upvotes"+" | "+str(comments)+" comments`"
 						update.message.reply_text(self_post_long, parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
 
-				elif not post['is_self']:
-					if post['over_18']:
+				else:
+					if post['over_18'] or 'post_hint' not in post.keys():
 						not_self_post = "`"+title+"\nby /u/"+poster+" on /r/"+sub+"\n<"+isNsfw+">" +" | "+str(up)+" upvotes"+" | "+str(comments)+" comments`"
 					else:
 						if len(title) > 57:
 							not_self_post = "`"+title+"\nby /u/"+poster+" - <"+isNsfw+">`"
 						else:
 							not_self_post = "`by /u/"+poster+" - <"+isNsfw+">`"
-					update.message.reply_text(not_self_post, parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
-				else:
-					pass
+					if 'post_hint' in post.keys():
+						if post['post_hint'] == 'image' and not post['over_18']:
+							bot.send_photo(cid, post['url'], caption=not_self_post.replace('`', '').replace('/u/',''), parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
+						elif (post['post_hint'] == 'link' or post['post_hint'] == 'rich:video') and not post['over_18']:
+							if "gifv" in post['url']:
+								url, title = getImgur(post['url'])
+								bot.send_document(cid, url, caption=not_self_post.replace('`', '').replace('/u/',''), parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
+							elif "gfycat" in post['url']:
+								url, title = getGfy(post['url'])
+								bot.send_document(cid, url, caption=not_self_post.replace('`', '').replace('/u/',''), parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
+							else:
+								url = post['url']
+								# I know that this looks awful
+								update.message.reply_text(not_self_post.replace('by /u/', '`[' + post['domain'] + ' link](' + url + ')`\nby /u/'), parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
+								# In fact, this whole section is a disaster
+								# This is barely readable in case it is, Im sorry and I appreciate you if you understand it
+						else:
+							update.message.reply_text(not_self_post, parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
+					else:
+						update.message.reply_text(not_self_post, parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
+
+
 			except:
 				pass
 		else:
