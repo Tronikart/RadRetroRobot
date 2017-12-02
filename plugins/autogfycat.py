c8e57a112f2114fbd85032ec31943fd7e9aeb3c4
Abraham Nassiff <anassiff@gmail.com>
2017-12-02 17:44:30 -0400

    Ugly work around the way Telegram handles documents

--- plugins/autogfycat.py
+++ plugins/autogfycat.py
@@ -5,11 +5,29 @@ print ('loading ' + __name__)
 def action(bot, update):
 	cid = getCID(update)
 	try:
-		url, title = getGfy(update.message.text)
-		bot.send_document(cid, url, caption=title)
+		url, url2, url3, title = getGfy(update.message.text)
+		# Thank you telegram for making me do this
+		# Fix your documentation please
+		if title:
+			try:
+				bot.send_document(cid, url, caption=title)
+			except:
+				try:
+					bot.send_document(cid, url2, caption=title)
+				except:
+					bot.send_document(cid, url3, caption=title)
+
+		else:
+			try:
+				bot.send_document(cid, url)
+			except:
+				try:
+					bot.send_document(cid, url2)
+				except:
+					bot.send_document(cid, url3)
 	except:
 		pass
 
-info = {	'triggers' 	:	r'.+gfycat.com.+',
+info = {	'triggers' 	:	r'.*gfycat\.com.*',
 			'active'	: 	True,
 			'admin'		: 	False}
