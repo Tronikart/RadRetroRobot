a3ee86c89f05d3d8087e39ccf73a9b97d0437647
Abraham Nassiff <anassiff@gmail.com>
2017-12-05 14:38:48 -0400

    Added extra output, changed BsF. to Bs.

--- plugins/dt.py
+++ plugins/dt.py
@@ -12,11 +12,12 @@ def action(bot, update, args):
 		dolartoday_usd = data['USD']['dolartoday']
 		implicito_usd = data['USD']['efectivo']
 		simadi_usd = data['USD']['sicad2']
+		btc = data['USD']['bitcoin_ref']
 		dolartoday_eur = data['EUR']['dolartoday']
 		implicito_eur = data['EUR']['efectivo']
 		simadi_eur = data['EUR']['sicad2']
 		date = data['_timestamp']['fecha']
-		bot.send_message(cid, u"`$ Dolar Today, {date}\n > USD\nDolar Today: BsF. {dolartoday_usd}\nImplicito: BsF. {implicito_usd}\nSimadi: BsF. {simadi_usd}\n> EUR \nDolar Today: BsF. {dolartoday_eur}\nImplicito: BsF. {implicito_eur}\nSicad: BsF. {simadi_eur}`".format(date=date, dolartoday_usd=dolartoday_usd, implicito_usd=implicito_usd, simadi_usd=simadi_usd, dolartoday_eur=dolartoday_eur, implicito_eur=implicito_eur, simadi_eur=simadi_eur), parse_mode="Markdown")
+		bot.send_message(cid, u"`$ Dolar Today, {date}\n > USD\nDolar Today: Bs. {dolartoday_usd}\nImplicito: Bs. {implicito_usd}\nSimadi: Bs. {simadi_usd}\nDolar Bitcoin: Bs. {btc}\n> EUR \nDolar Today: Bs. {dolartoday_eur}\nImplicito: Bs. {implicito_eur}\nSicad: Bs. {simadi_eur}`".format(date=date, dolartoday_usd=dolartoday_usd, implicito_usd=implicito_usd, simadi_usd=simadi_usd, dolartoday_eur=dolartoday_eur, implicito_eur=implicito_eur, simadi_eur=simadi_eur, btc=btc), parse_mode="Markdown")
 	else:
 		update.message.reply_text("`There has been an error, the number {error} to be specific.`".format(error=request.status_code), parse_mode="Markdown", reply_to_message_id=update.message.message_id)
 
