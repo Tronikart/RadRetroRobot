2c1348d0c5d333441eae7db901acbb2747d0f5e4
Abraham Nassiff <anassiff@gmail.com>
2018-03-14 20:15:56 -0400

    Now consuming graph API instead of scrapping Facebook and hoping for the best

--- plugins/autoFacebookVideo.py
+++ plugins/autoFacebookVideo.py
@@ -3,25 +3,22 @@ from utils import *
 print ('loading ' + __name__)
 
 def action(bot, update):
-	print("beep")
 	cid = getCID(update)
+	token = getFBToken()
 	try:
-		cid = getCID(update)
-		url = re.findall(r'(http[s]?://www.facebook.com/.*/videos/\d.+[/\S+]?)', update.message.text)[0]
-		url = url.replace('www', 'm')
-		soup = makesoup(url)
-		for link in soup.find_all('a'):
-			if "/video" in link['href']:
-				mp4url = link['href']
-				break
-    
-    # I know this looks ugly
-		mp4url = re.findall(r'src=(.*)$', mp4url)[0].replace('%3A', ':').replace('%2F', '/').replace('%3F', '?').replace('%3D', '=').replace('%26', '&')
-		bot.send_document(cid, mp4url)
-	except:
-		pass
+		request_url = 'https://graph.facebook.com/'
+		url = re.findall(r'(.*http[s]?://www.facebook.com/(groups|.*)/(videos|\d.+)/\d.+/.*)', update.message.text)[0][0]
 
-info = {	'triggers' 	:	r'.*http[s]?://www.facebook.com/.*/videos/\d.+/.*',
+		# Getting the post ID from either a /video/ url or a /groups/ url
+		media_id = url.split('/')[5] if url.split('/')[4] == "videos" else url.split('/')[6]
+
+		request = requests.get(request_url + media_id, {'access_token': token, 'fields' : 'source'})
+		mp4url = request.json()['source']
+		bot.send_document(cid, mp4url)
+	except Exception as e:
+		raise e
+	
+
+info = {	'triggers' 	:	r'.*http[s]?://www.facebook.com/(groups|.*)/(videos|\d.+)/\d.+/.*',
 			'active'	: 	True,
 			'admin'		: 	False}
-
