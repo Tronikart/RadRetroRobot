230b6e2fb6c6c568493e0cfef5a1df7561077fbe
Abraham Nassiff <anassiff@gmail.com>
2018-03-18 19:14:58 -0400

    Fixed and improved quoted tweet management

--- plugins/autotweet.py
+++ plugins/autotweet.py
@@ -8,6 +8,7 @@ def action(bot, update):
 	cid = getCID(update)
 	try:
 		status = api.get_status(tweet_id[0], tweet_mode='extended', include_entities=True)
+		print (status)
 		tweet = status.full_text
 		favs = str(status.favorite_count)
 		RTs = str(status.retweet_count)
@@ -26,27 +27,27 @@ def action(bot, update):
 			photos, videos = urlsForAlbum(urls)
 		except:
 			urls = False
-
 	except:
 		pass
 	try:
 		try:
-			quoted_tweet = status.quoted_status["text"]
+			quoted_tweet = status.quoted_status["full_text"]
 			quoted_user = status.quoted_status["user"]["screen_name"]
 			quoted_username = status.quoted_status["user"]["name"]
 			url = treatLink("https://twitter.com/" + quoted_username)
-			update.message.reply_text("`_______quoted tweet_______`\n  [" + quoted_username + " - @" + quoted_user + "](" + url + ")\n" + quoted_tweet + "`\n_____end of quoted tweet_____`\n\n`RTs:{" + RTs + "} | Likes: " + favs + "`", parse_mode="Markdown", reply_to_message_id=update.message.message_id)
-		except:
+			update.message.reply_text("[" + username + " - @" + user + "](" + url + ")\n" + tweet + "\n\n`_______quoted tweet_______`\n[" + quoted_username + " - @" + quoted_user + "](" + url + ")\n" + quoted_tweet + "`\n_____end of quoted tweet_____`\n\n`RTs:" + RTs + " | Likes: " + favs + "`", parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
+		except Exception as e:
 			if len(tweet) < 110:
 				if media:
 					update.message.reply_text("[" + username + " - @" + user + "](" + url + ")" + "\n`RTs:" + RTs + " | Likes: " + favs +"`", parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
 					bot.send_document(cid, media)
-				elif urls and len(urls) > 1:
+				elif urls and len(urls) > 1 :
 					update.message.reply_text("[" + username + " - @" + user + "](" + url + ")" + "\n`RTs:" + RTs + " | Likes: " + favs +"`", parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
 					if photos:
 						sendAlbums("photo", photos, cid)
 					if videos:
 						sendAlbums("video", videos, cid)
+
 				else:
 					update.message.reply_text("[" + username + " - @" + user + "](" + url + ")" + "\n`RTs:" + RTs + " | Likes: " + favs +"`", parse_mode="Markdown", reply_to_message_id=update.message.message_id, disable_web_page_preview=True)
 			else:
@@ -68,4 +69,5 @@ def action(bot, update):
 info = {	'triggers' 	:	r'.*http[s]?://twitter\.com.+/status/\d+.*',
 			'active'	: 	True,
 			'admin'		: 	False}
+
 
